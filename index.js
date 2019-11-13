d656147d6141513264aa5268b3f562e6098b59e5
louis baptiste <louisvis@ymail.com>
2019-11-13 04:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
+=======
+let companyName = 'Scuber';
+>>>>>>> 116e9ebc53aeef0ea5e6ef196cbf46e0f2397380
 let mostProfitableNeighborhood ='Chelsea'
 let companyCeo = 'Susan Smith'
